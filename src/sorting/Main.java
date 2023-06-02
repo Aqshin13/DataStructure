05deac15cc6e6e9a8a5663d1eec9db3414c3a144
“Aqshin13” <aqsin.quliyev.17@inbox.ru>
2023-06-02 13:24:07 +0400

    Insertion Sort

--- src/sorting/Main.java
+++ src/sorting/Main.java
@@ -14,6 +14,19 @@ public class Main {
             }
         }
     }
+    
+    public static void insertSort(int[] arr) {
+        int temp = 0;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
 
