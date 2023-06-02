88b21bf25741b747343429632189ab3e11afd02c
“Aqshin13” <aqsin.quliyev.17@inbox.ru>
2023-06-02 14:33:38 +0400

    Selection Sort

--- src/sorting/Main.java
+++ src/sorting/Main.java
@@ -27,6 +27,23 @@ public class Main {
             }
         }
     }
+    
+     public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+
+    }
+
 
     public static void main(String[] args) {
 
