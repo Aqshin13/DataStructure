00ed34cc6b51c937615681919b867eeb6c9a9cf9
“Aqshin13” <aqsin.quliyev.17@inbox.ru>
2023-06-02 11:28:14 +0400

    Bubble sorting

--- src/sorting/Main.java
+++ src/sorting/Main.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sorting;
 
-/**
- *
- * @author agshin
- */
 public class Main {
-    
+
+    public static void bubbleSort(int[] arr){
+        int temp = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-1-i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+
+
+    }
 }
